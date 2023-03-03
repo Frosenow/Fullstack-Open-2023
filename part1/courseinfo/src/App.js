8d752d012d535495e30da39cfa58929d7f0acaa8
Frosenow <filip.potepa@gmail.com>
2023-03-03 17:40:43 +0100

    step2 completed

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,7 +4,27 @@ function Header(props){
   )
 }
 
-function Content(props){
+function Content({courseParts}){
+
+  return (
+  <div>
+    <Part 
+      part={courseParts.part[0]}
+      exercise={courseParts.exercise[0]}
+    />
+    <Part 
+      part={courseParts.part[1]}
+      exercise={courseParts.exercise[1]}
+    />
+    <Part 
+      part={courseParts.part[2]}
+      exercise={courseParts.exercise[2]}
+    />
+  </div>
+  )
+}
+
+function Part(props){
   return (
     <p>
       {props.part} {props.exercise}
@@ -21,29 +41,16 @@ function Total(props){
 
 function App() {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const courseParts = {
+    part: ['Fundamentals of React', 'Using props to pass data', 'State of a component'],
+    exercise: [10, 7, 14]
+  }
   return (
     <div>
       <Header course={course}/>
-      <Content 
-        part={part1}
-        exercise={exercises1}
-      />
-      <Content 
-        part={part2}
-        exercise={exercises2}
-      />
-      <Content 
-        part={part3}
-        exercise={exercises3}
-      />
+      <Content courseParts={courseParts}/>
       <Total 
-        exercises={[exercises1, exercises2, exercises3]}
+        exercises={courseParts.exercise}
       />
     </div>
   );
