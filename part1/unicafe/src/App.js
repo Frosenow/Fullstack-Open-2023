7d51cb570ee5fe310f3d13a69dcff2f7fa28f2a2
Frosenow <filip.potepa@gmail.com>
2023-04-08 01:01:36 +0200

    unicafe step2 completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -23,17 +23,42 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [sum, setSum] = useState(0);
+
+  const handleGoodClick = () => {
+    let goodValue = good + 1;
+    setGood(goodValue);
+    let newSum = sum + 1;
+    setSum(newSum);
+  };
+
+  const handleNeutralClick = () => {
+    let neutralValue = neutral + 1;
+    setNeutral(neutralValue);
+    let newSum = sum + 1;
+    setSum(newSum);
+  };
+
+  const handleBadClick = () => {
+    let badValue = bad + 1;
+    setBad(badValue);
+    let newSum = sum + 1;
+    setSum(newSum);
+  };
 
   return (
     <>
       <Header text="Give feedback" />
-      <Button onClick={() => setGood(good + 1)} reaction="GOOD"></Button>
-      <Button onClick={() => setNeutral(neutral + 1)} reaction="NEUTRAL"></Button>
-      <Button onClick={() => setBad(bad + 1)} reaction="BAD"></Button>
+      <Button onClick={handleGoodClick} reaction="GOOD"></Button>
+      <Button onClick={handleNeutralClick} reaction="NEUTRAL"></Button>
+      <Button onClick={handleBadClick} reaction="BAD"></Button>
       <Header text="Statistic" />
       <Statistic text="good" value={good} />
       <Statistic text="neutral" value={neutral} />
       <Statistic text="bad" value={bad} />
+      <Statistic text="all" value={good + neutral + bad} />
+      <Statistic text="average" value={(good + neutral + bad) / 3} />
+      <Statistic text="positive" value={good > 0 ? `${(good / sum) * 100}%` : `0%`} />
     </>
   );
 };
