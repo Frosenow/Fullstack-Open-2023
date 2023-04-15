23dc244ca70c0c5dedee0a85b3af1e72b59d32f6
Frosenow <filip.potepa@gmail.com>
2023-04-15 22:33:20 +0200

    phonebook step4 added

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,9 +3,14 @@ import { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+  const [filterPersons, setFilterPersons] = useState([...persons]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   function handleInputChange(e) {
     setNewName(e.target.value);
@@ -32,6 +37,7 @@ const App = () => {
         id: persons.length + 1,
       };
       setPersons(persons.concat(names));
+      setFilterPersons(persons.concat(names));
     } else {
       alert(`${newName} is already added to phonebook`);
     }
@@ -39,9 +45,22 @@ const App = () => {
     setNewNumber("");
   }
 
+  function filterData(e) {
+    setFilter(e.target.value);
+    const searchTerm = e.target.value.toLowerCase();
+    const personsFilter = persons.filter((person) =>
+      person.name.toLowerCase().includes(searchTerm)
+    );
+    setFilterPersons(personsFilter);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={filterData} value={filter} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleForm}>
         <div>
           name:
@@ -59,10 +78,10 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => {
+      {filterPersons.map((filterPersons) => {
         return (
-          <li key={person.id}>
-            {person.name} {person.number}
+          <li key={filterPersons.id}>
+            {filterPersons.name} {filterPersons.number}
           </li>
         );
       })}
